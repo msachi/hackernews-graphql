f6f5d877d317fbd4549e5123644a97f40e9dba7a
Marko Sustarsic <MarkoS@canneslions.com>
2018-02-06 14:32:11 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import registerServiceWorker from './registerServiceWorker';
+
+import App from './components/App';
+import './index.css';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root')
+);
+
+registerServiceWorker();

b229e08a4f0b9053f9c8d9637874ec813a3325c0
Marko Sustarsic <MarkoS@canneslions.com>
2018-02-06 16:01:02 +0000

    Steps up to & including filtering

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
+import LinkList from './LinkList';
 import Login from './Login';
 import CreateLink from './CreateLink';
-import LinkList from './LinkList';
+import Search from './Search';
 
 class App extends Component {
   render() {
@@ -12,9 +13,10 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
+            <Route exact path="/" component={LinkList} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/create" component={CreateLink} />
-            <Route exact path="/" component={LinkList} />
+            <Route exact path='/search' component={Search}/>
           </Switch>
         </div>
       </div>
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -18,28 +18,49 @@ class LinkList extends Component {
 
     return (
       <div>
-        {linksToRender.map(link => (
+        {linksToRender.map((link, index) => (
           <Link
             key={link.id}
+            index={index}
             link={link}
+            updateStoreAfterVote={this._updateCacheAfterVote}
            />
         ))}
       </div>
     );
   };
+
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data });
+  }
 }
 
-const FEED_QUERY = gql`
- query FeedQuery {
-   feed {
-     links {
-       id
-       createdAt
-       url
-       description
-     }
-   }
- }
+export const FEED_QUERY = gql`
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
 `;
 
 export default graphql(
